7d5a42d46f1f50051bffd7c5ef3a3bc6182ff263
Amanda Souza <amandasjc17@gmail.com>
2025-11-17 21:20:45 -0300

    update implementation

--- src/application/Main.java
+++ src/application/Main.java
@@ -34,5 +34,11 @@ public class Main {
         Seller newSeller = new Seller(null, "Jonas", "jonas@gmail.com", new Date(), 4000.0, department);
         sellerDao.insert(newSeller);
         System.out.println("Inserted! New id = " + newSeller.getId());
+
+        System.out.println("\n=== TEST 5: seller update ===");
+        seller = sellerDao.findById(1);
+        seller.setName("Joaquina Souza");
+        sellerDao.update(seller);
+        System.out.println("Update completed");
     }
 }
